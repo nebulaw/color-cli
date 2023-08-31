4185b307da2283d7cfb115f1af5fa0c272ea43cd
nebulaw <karlo.tsutskiridze@gmail.com>
2023-08-31 05:53:17 +0400

    feat: saturate, lighten, brighten and rotate ops support

--- README.md
+++ README.md
@@ -19,51 +19,71 @@ sudo ./install.sh /usr/bin/
 ```
 
 
-## Usage
-Currently, supported color models are: RGB, HEX, HSL and HSV. Here is an example of creating RGB color:
+## Usage/Examples
+Currently, supported color models are RGB, HEX, HSL, and HSV.
+
+Let's say, we want to preview `#00008b` - the color of a rainy sky:
 ```bash
-color rgb 244 180 220
+color hex \#00008b
 ```
 ![output-image-1][image-1]
-As you can see from the output, it is a pinkish color.
+
+Hex also starts with `0x` or `0X`  along with `#`.
+
+To create RGB color, we can type:
+```bash
+color rgb [0-255] [0-255] [0-255]
+```
 
 For HSV and HSL:
 ```bash
-color [hsv|hsl] [0-359] [0-100] [0-100]
+color [hsv|hsl] [0-359.0] [0-100.0] [0-100.0]
 ```
 
-HEX colors can have different prefixes such as: *0x, 0X, and #*:
+**Supported operations:**
+
+|  Operation | Option           | Parameter                       |
+| :--------  | :--------------- | :------------------------------ |
+|  Negate    | `-n, --negate`   | Takes no parameter              |
+|  Convert   | `-t, --to`       | `rgb`, `hex`, `hsv`, and `hsl`  |  
+|  Rotate    | `-r, --rotate`   |  Floating point with no range   |
+|  Saturate  | `-s, --saturate` |  Between `-100.0` and `100.0`   |
+|  Lighten   | `-l, --lighten`  |  Between `-100.0` and `100.0`   |
+|  Brighten  | `-b, --brighten` |  Between `-100.0` and `100.0`   |
+
+**Examples**
+
 ```bash
-color hex \#F8B195
-color hex 0x6c5b7b
-color hex 0X355C7D
+color hsv 266 80 80 --lighten -20 --to rgb
 ```
-![output-image-2][image-2]
+![output-image-3][image-2]
 
-Here are some operations that are supported at this moment:
 ```bash
-# negation of hex value
-color hex 0x0011FF [--negate|-n]
-
-# converting hex color to hsv
-color hex 0x0011FF [--to|-t] [rgb|hsv|hsl]
+color hex 0x00008b --rotate +245
 ```
 ![output-image-3][image-3]
+
+```bash
+color hex 0x00008b --rotate -40 --saturate -11.8 --to hsl
+```
+![output-image-3][image-4]
 
 You can try ```color --help``` for more options.
 
 
 ## Contributing
-Contributions are welcome. If you find any bug or have an idea for
-improvements, feel free to open an issue or send a pull request.
+Contributions are always welcome. If you report any bugs or have ideas for
+improvements, please don't hesitate to open an issue or send a pull request.
 
 
 ## License
-color-cli is licensed under the MIT License - see
+Color-cli is licensed under the MIT License - see
 [LICENSE][license] for details.
 
 
 [license]: https://github.com/nebulaw/color-cli/blob/main/LICENSE
-[image-1]: https://raw.githubusercontent.com/nebulaw/color-cli/main/images/screenshot1.png
-[image-2]: https://raw.githubusercontent.com/nebulaw/color-cli/main/images/screenshot2.png
-[image-3]: https://raw.githubusercontent.com/nebulaw/color-cli/main/images/screenshot3.png
+[image-1]: https://raw.githubusercontent.com/nebulaw/color-cli/main/images/image-1.png
+[image-2]: https://raw.githubusercontent.com/nebulaw/color-cli/main/images/image-2.png
+[image-3]: https://raw.githubusercontent.com/nebulaw/color-cli/main/images/image-3.png
+[image-4]: https://raw.githubusercontent.com/nebulaw/color-cli/main/images/image-4.png
+
--- color.c
+++ color.c
@@ -11,19 +11,39 @@
 #define MAX3(a, b, c)   MAX2((a), MAX2((b), (c)))
 #define MIN3(a, b, c)   MIN2((a), MIN2((b), (c)))
 #define ROUND(x)        ((x) >= 0 ? floor((x) + 0.5) : ceil((x) - 0.5))
+#define getVarName(var) #var
 
 #define MAXCLOPS        20
 
+enum MODEL_E {
+    RGB = 2,
+    HEX = 4,
+    HSV = 8,
+    HSL = 16,
+};
+
+enum CLOP_E {
+    NEGATE      = 128,
+    CONVERSION  = 256,
+    SATURATE    = 512,
+    ROTATE      = 1024,
+    LIGHTEN     = 4096,
+    BRIGHTEN    = 8192,
+};
+
 typedef struct {
-  int model;
-  float values[4];
+    int model;
+    float values[4];
 } color_t;
 
-enum {
-  RGB = 2, HEX = 4, HSV = 8, HSL = 16,
-  /* clops */
-  NEGATE = 128, CONVERSION = 256
-};
+typedef struct {
+    int op;
+    int has_param;
+    float param;
+} clop_t;
+
+
+clop_t createclop(int op, int requires_param, char *value);
 
 color_t creatergb(char *red, char *green, char *blue);
 color_t createhex(char *hex);
@@ -52,7 +72,12 @@ static color_t negatehex(color_t hex);
 static color_t negatehsv(color_t hsv);
 static color_t negatehsl(color_t hsl);
 
-void displaycolor(color_t color, int options);
+color_t saturate(color_t color, float value);
+color_t rotate(color_t color, float value);
+color_t lighten(color_t color, float value);
+color_t brighten(color_t color, float value);
+
+void displaycolor(color_t color);
 static void displayrgb(color_t rgb);
 static void displayhex(color_t hex);
 static void displayhsv(color_t hsv);
@@ -61,16 +86,20 @@ static void displayhsl(color_t hsv);
 void usage(void);
 
 static struct option arg_options[] = {
-  {"negate", no_argument, 0, 'n'},
-  {"to", required_argument, 0, 't'},
-  {"saturate", required_argument, 0, 's'},
-  {0, 0, 0, 0}
+  { "negate",     no_argument,        0,  'n' },
+  { "to",         required_argument,  0,  't' },
+  { "saturate",   required_argument,  0,  's' },
+  { "rotate",     required_argument,  0,  'r' },
+  { "lighten",    required_argument,  0,  'l' },
+  { "brighten",   required_argument,  0,  'b' },
+  { 0, 0, 0, 0 }
 };
 
+/* TODO: impl cmyk color model */
 int main(int argc, char **argv)
 {
-  // NOTE: I see no reason of `OPTIONS`, as we have clops
-  int COLOR_MODEL = RGB, OPTIONS = 0, CLOPS[MAXCLOPS], clp = 0;
+  clop_t CLOPS[MAXCLOPS];
+  int COLOR_MODEL = RGB, clp = 0;
   char *var1, *var2, *var3;
 
   char **argv_ptr = argv;
@@ -87,6 +116,7 @@ int main(int argc, char **argv)
     exit(0);
   }
 
+  /* parse color model */
   COLOR_MODEL = strcmp(*argv, "rgb") == 0 ? RGB :
     strcmp(*argv, "hex") == 0 ? HEX :
     strcmp(*argv, "hsv") == 0 ? HSV :
@@ -98,6 +128,7 @@ int main(int argc, char **argv)
     exit(1);
   }
 
+  /* parse color values */
   if (strcmp(*argv, "hex") == 0) {
     if (*(++argv) == NULL) {
       fprintf(stderr, "color: invalid %s color format.\n", *(--argv));
@@ -124,28 +155,15 @@ int main(int argc, char **argv)
 
   /* parse options */
   int op;
-  while ((op = getopt_long(argc, argv_ptr, "nt:", arg_options, NULL)) != -1) {
+  while ((op = getopt_long(argc, argv_ptr, "ns:r:l:b:t:", arg_options, NULL)) != -1) {
     switch (op) {
-    case 'n':
-      OPTIONS |= NEGATE;
-      CLOPS[clp++] = NEGATE;
-      break;
-    case 't':
-      OPTIONS |= CONVERSION;
-      if (!strcmp(optarg, "hex")) OPTIONS |= HEX;
-      else if (!strcmp(optarg, "rgb")) OPTIONS |= RGB;
-      else if (!strcmp(optarg, "hsv")) OPTIONS |= HSV;
-      else if (!strcmp(optarg, "hsl")) OPTIONS |= HSL;
-      else {
-        fprintf(stderr, "color: invalid convert type %s.\n", optarg);
-        fprintf(stderr, "Try 'color --help' for more options.\n");
-        exit(-1);
-      }
-      break;
-    case 's':
-    case '?':
-      /* fprintf(stderr, "Invalid option %c.\n", optopt); */
-      exit(-1);
+    case 'n': CLOPS[clp++] = createclop(NEGATE, 0, ""); break;
+    case 's': CLOPS[clp++] = createclop(SATURATE, 1, optarg); break;
+    case 'r': CLOPS[clp++] = createclop(ROTATE, 1, optarg); break;
+    case 'l': CLOPS[clp++] = createclop(LIGHTEN, 1, optarg); break;
+    case 'b': CLOPS[clp++] = createclop(BRIGHTEN, 1, optarg); break;
+    case 't': CLOPS[clp++] = createclop(CONVERSION, 1, optarg); break;
+    case '?': exit(-1);
     default: break;
     }
   }
@@ -160,10 +178,20 @@ int main(int argc, char **argv)
   case HSL: color = createhsl(var1, var2, var3); break;
   }
 
-  if ((OPTIONS & NEGATE) == NEGATE) color = negate(color);
+  /* apply clops */
+  for (int i = 0; i < clp; i++) {
+    switch (CLOPS[i].op) {
+    case NEGATE:      color = negate(color); break;
+    case CONVERSION:  color = convert(color, (int) CLOPS[i].param); break;
+    case SATURATE:    color = saturate(color, CLOPS[i].param); break;
+    case ROTATE:      color = rotate(color, CLOPS[i].param); break;
+    case LIGHTEN:     color = lighten(color, CLOPS[i].param); break;
+    case BRIGHTEN:    color = brighten(color, CLOPS[i].param); break;
+    }
+  }
 
   /* display the result*/
-  displaycolor(color, OPTIONS);
+  displaycolor(color);
 
   return 0;
 }
@@ -173,6 +201,31 @@ int main(int argc, char **argv)
       printf(args); \
       exit(1); \
     }
+
+clop_t createclop(int op, int has_param, char *value)
+{
+  float val;
+  if (has_param && op != CONVERSION) {
+    val = strspn(value, "+-.0123456789") == strlen(value) ? atof(value) : -101;
+    if ((val < -100 || 100 < val) && op != ROTATE)
+      EXIT_ON_FAILURE("invalid value: %s", value)
+  } else if (has_param && op == CONVERSION) {
+    if (strcmp(value, "rgb") == 0) val = RGB;
+    else if (strcmp(value, "hex") == 0) val = HEX;
+    else if (strcmp(value, "hsv") == 0) val = HSV;
+    else if (strcmp(value, "hsl") == 0) val = HSL;
+    else {
+      fprintf(stderr, "color: invalid convert type %s.\n", optarg);
+      fprintf(stderr, "Try 'color --help' for more options.\n");
+      exit(-1);
+    }
+  }
+  clop_t clop;
+  clop.op = op;
+  clop.has_param = has_param;
+  clop.param = val;
+  return clop;
+}
 
 color_t creatergb(char *red, char *green, char *blue)
 {
@@ -211,7 +264,7 @@ color_t createhex(char *hex)
   }
   /* validate actual value */
   if (strspn(hexvalue, "0123456789abcdefABCDEF") != strlen(hexvalue) || strlen(hexvalue) != 6)
-    EXIT_ON_FAILURE("invalid hex format: %s", hex)
+    EXIT_ON_FAILURE("invalid hex format: %s\n", hex)
   int r, g, b;
   sscanf(hexvalue, "%02x%02x%02x", &r, &g, &b);
   color_t hex_color;
@@ -244,12 +297,12 @@ color_t createhsl(char *hue, char *saturation, char *lightness)
   float s = strspn(saturation, ".0123456789") == strlen(saturation) ? atof(saturation) : -1;
   float l = strspn(lightness, ".0123456789") == strlen(lightness) ? atof(lightness) : -1;
   /* range validation */
-  if (!(0 <= h && h < 360)) EXIT_ON_FAILURE("invalid hue value: %s\n", hue)
+  if (h < 0) EXIT_ON_FAILURE("invalid hue value: %s\n", hue)
   if (!(0 <= s && s <= 100)) EXIT_ON_FAILURE("invalid saturation value: %s\n", saturation)
   if (!(0 <= l && l <= 100)) EXIT_ON_FAILURE("invalid lightness value: %s\n", lightness)
   color_t hsl_color;
   hsl_color.model = HSL;
-  hsl_color.values[0] = h;
+  hsl_color.values[0] = fmodf(h, 360.f);
   hsl_color.values[1] = s;
   hsl_color.values[2] = l;
   hsl_color.values[3] = 255;
@@ -319,9 +372,9 @@ static color_t rgb2hsv(color_t rgb)
 {
   float r = rgb.values[0] / 255.f, g = rgb.values[1] / 255.f, b = rgb.values[2] / 255.f;
   float max = MAX3(r, g, b), min = MIN3(r, g, b);
-  float hue = fmod((hue =
+  float hue = fmodf((hue =
     max == min ? 0.f :
-    max == r ? 60.f * (fmod((g - b), 6.f) / (max - min)) :
+    max == r ? 60.f * (fmodf((g - b), 6.f) / (max - min)) :
     max == g ? 60.f * (2.f + (b - r) / (max - min)) :
 /* max == b */ 60.f * (4.f + (r - g) / (max - min))) < 0.f ?
       hue + 360 : hue, 360.f);
@@ -339,9 +392,9 @@ static color_t rgb2hsl(color_t rgb)
 {
   float r = rgb.values[0] / 255.f, g = rgb.values[1] / 255.f, b = rgb.values[2] / 255.f;
   float max = MAX3(r, g, b), min = MIN3(r, g, b);
-  float hue = fmod((hue =
+  float hue = fmodf((hue =
     max == min ? 0.f :
-    max == r ? 60.f * (fmod((g - b), 6.f) / (max - min)) :
+    max == r ? 60.f * (fmodf((g - b), 6.f) / (max - min)) :
     max == g ? 60.f * (2.f + (b - r) / (max - min)) :
 /* max == b */ 60.f * (4.f + (r - g) / (max - min))) < 0.f ?
       hue + 360 : hue, 360.f);
@@ -359,6 +412,7 @@ static color_t rgb2hsl(color_t rgb)
 static color_t hex2rgb(color_t hex)
 {
   color_t rgb_color;
+  rgb_color.model = RGB;
   rgb_color.values[2] = (int)roundf(hex.values[0]) & 0xFF;
   rgb_color.values[1] = ((int)roundf(hex.values[0]) >> 8) & 0xFF;
   rgb_color.values[0] = ((int)roundf(hex.values[0]) >> 16) & 0xFF;
@@ -462,17 +516,16 @@ color_t negate(color_t color)
   case HEX: return negatehex(color);
   case HSV: return negatehsv(color);
   case HSL: return negatehsl(color);
-  default:
-    EXIT_ON_FAILURE("invalid negation\n")
+  default: EXIT_ON_FAILURE("invalid color model\n")
   }
 }
 
 static color_t negatergb(color_t rgb)
 {
   color_t negated_rgb = copy(rgb);
-  negated_rgb.values[0] = fmod(255 - negated_rgb.values[0], 256);
-  negated_rgb.values[1] = fmod(255 - negated_rgb.values[1], 256);
-  negated_rgb.values[2] = fmod(255 - negated_rgb.values[2], 256);
+  negated_rgb.values[0] = fmodf(255 - negated_rgb.values[0], 256);
+  negated_rgb.values[1] = fmodf(255 - negated_rgb.values[1], 256);
+  negated_rgb.values[2] = fmodf(255 - negated_rgb.values[2], 256);
   return negated_rgb;
 }
 
@@ -484,60 +537,69 @@ static color_t negatehex(color_t hex)
 static color_t negatehsv(color_t hsv)
 {
   color_t negated_hsv = copy(hsv);
-  negated_hsv.values[0] = fmod(negated_hsv.values[0] + 180.f, 360.f);
+  negated_hsv.values[0] = fmodf(negated_hsv.values[0] + 180.f, 360.f);
   return negated_hsv;
 }
 
 static color_t negatehsl(color_t hsl)
 {
   color_t negated_hsl = copy(hsl);
-  negated_hsl.values[0] = fmod(negated_hsl.values[0] + 180.f, 360.f);
+  negated_hsl.values[0] = fmodf(negated_hsl.values[0] + 180.f, 360.f);
   return negated_hsl;
 }
 
-#define NEGATE_OP(neg, code) neg == 1 ? negate(code) : code
-
-void displaycolor(color_t color, int options)
+color_t saturate(color_t color, float value)
 {
-  if ((options & CONVERSION) == CONVERSION) {
-    if ((options & HEX) == HEX) {
-      switch (color.model) {
-      case HEX: displayhex(color); break;
-      case RGB: displayhex(rgb2hex(color)); break;
-      case HSV: displayhex(hsv2hex(color)); break;
-      case HSL: displayhex(hsl2hex(color)); break;
-      }
-    } else if ((options & RGB) == RGB) {
-      switch (color.model) {
-      case RGB: displayrgb(color); break;
-      case HEX: displayrgb(hex2rgb(color)); break;
-      case HSV: displayrgb(hsv2rgb(color)); break;
-      case HSL: displayrgb(hsl2rgb(color)); break;
-      }
-    } else if ((options & HSV) == HSV) {
-      switch (color.model) {
-      case RGB: displayhsv(rgb2hsv(color)); break;
-      case HEX: displayhsv(hex2hsv(color)); break;
-      case HSV: displayhsv(color); break;
-      case HSL: displayhsv(hsl2hsv(color)); break;
-      }
-    } else if ((options & HSL) == HSL) {
-      switch (color.model) {
-      case RGB: displayhsl(rgb2hsl(color)); break;
-      case HEX: displayhsl(hex2hsl(color)); break;
-      case HSV: displayhsl(hsv2hsl(color)); break;
-      case HSL: displayhsl(color); break;
-      }
-    }
-  } else {
-    switch (color.model) {
-    case RGB: displayrgb(color); break;
-    case HEX: displayhex(color); break;
-    case HSV: displayhsv(color); break;
-    case HSL: displayhsl(color); break;
-    }
-  }
+  int color_model = color.model;
+  color = convert(color, HSL);
+  color.values[1] = value >= 0 ?
+    color.values[1] + (100.0f - color.values[1]) * (value / 100.0f) :
+    color.values[1] + color.values[1] * (value / 100.f);
+  color = convert(color, color_model);
+  return color;
+}
 
+color_t rotate(color_t color, float value)
+{
+  int color_model = color.model;
+  color = convert(color, HSL);
+  float new_hue = (new_hue = color.values[0] + value) < 0 ?
+                  ((int)(-new_hue) / 360 + 1) * 360 + new_hue : new_hue;
+  color.values[0] = fmodf(new_hue, 360);
+  color = convert(color, color_model);
+  return color;
+}
+
+color_t lighten(color_t color, float value)
+{
+  int color_model = color.model;
+  color = convert(color, HSL);
+  color.values[2] = value >= 0 ?
+    color.values[2] + (100.0f - color.values[2]) * (value / 100.0f) :
+    color.values[2] + color.values[2] * (value / 100.f);
+  color = convert(color, color_model);
+  return color;
+}
+
+color_t brighten(color_t color, float value)
+{
+  int color_model = color.model;
+  color = convert(color, HSV);
+  color.values[2] = value >= 0 ?
+    color.values[2] + (100.0f - color.values[2]) * (value / 100.0f) :
+    color.values[2] + color.values[2] * (value / 100.f);
+  color = convert(color, color_model);
+  return color;
+}
+
+void displaycolor(color_t color)
+{
+  switch (color.model) {
+  case RGB: displayrgb(color); break;
+  case HEX: displayhex(color); break;
+  case HSV: displayhsv(color); break;
+  case HSL: displayhsl(color); break;
+  }
 }
 
 #define RGB_PREFIX(rgb) \
@@ -566,7 +628,7 @@ static void displayhsv(color_t hsv)
 {
   color_t rgb = hsv2rgb(hsv);
   RGB_PREFIX(rgb);
-  printf("  hsv(%.2f, %.2f, %.2f)  ", hsv.values[0], hsv.values[1], hsv.values[2]);
+  printf("  hsv(%.2f, %.2f%%, %.2f%%)  ", hsv.values[0], hsv.values[1], hsv.values[2]);
   RGB_SUFFIX;
 }
 
@@ -574,7 +636,7 @@ static void displayhsl(color_t hsl)
 {
   color_t rgb = hsl2rgb(hsl);
   RGB_PREFIX(rgb);
-  printf("  hsl(%.2f, %.2f, %.2f)  ", hsl.values[0], hsl.values[1], hsl.values[2]);
+  printf("  hsl(%.2f, %.2f%%, %.2f%%)  ", hsl.values[0], hsl.values[1], hsl.values[2]);
   RGB_SUFFIX;
 }
 
@@ -583,15 +645,21 @@ void usage(void)
   puts("Usage: color [model] [values] [options]\n");
   puts("Examples:");
   puts("    color rgb 244 180 220 -nt hex");
-  puts("    color hex 0xA122FF\n");
+  puts("    color hex 0xA122FF -r -20 --saturate 23\n");
   puts("Models:");
-  puts("    rgb           RGB values are between 0-255");
-  puts("    hex           HEX can be 3-bit or 6-bit integer. Prefixes: 0x, 0X, #");
-  puts("    hsv           Hue is 0-359deg, S, V - 0-100%");
-  puts("    hsl           Same specifications as HSV color model\n");
+  puts("    rgb             RGB values are between 0-255.");
+  puts("    hex             HEX can be 3-bit or 6-bit integer. valid: 0x, 0X, #.");
+  puts("    hsv             Hue is 0-359deg, S, V - 0-100%.");
+  puts("    hsl             Same specifications as HSV color model.\n");
   puts("Options:");
-  puts("    -n, --negate  Negate value");
-  puts("    -t, --to      Convert to the specific color model");
+  puts("    -n, --negate    Output negative value.");
+  puts("    -t, --to        Convert to the specific color model.");
+  puts("    -r, --rotate    Rotate the hue value of color, parameter can be");
+  puts("                        negative or positive float.");
+  puts("    -s, --saturate  Saturate color, parameter is between -100 and 100.");
+  puts("    -l, --lighten   Increase or decrease luminosity of color. Range is");
+  puts("                        between -100 and 100.");
+  puts("    -b, --brighten  Control brightness. Range is between -100 and 100");
 }
 
 /* EOF */
